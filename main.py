c35f1a56944e6778bdde736b83e45906aa95fa87
Andrew Brown <salvor7@gmail.com>
2016-06-03 12:17:43 -0700

    Wired in the Go game state for the Position object
    Added a Listproperty
    which is updated with each move,
    which triggers the on_gamestate method
    which updates what is shown in each intersection button

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.logger import Logger
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, ListProperty
 from kivy.clock import Clock
 
 import go
@@ -22,22 +22,38 @@ class Board(FloatLayout):
             self.method1()
         except AttributeError:
             Clock.schedule_once(self.method1)
-        self.state = go.Position()
-            
+
     def method1(self, inst=None, value=None):
         Logger.info(str(self.background.texture))
 
         
 class ButtonGrid(GridLayout):
-    
+    gamestate = ListProperty([])
+
     def __init__(self, **kwargs):
         super(ButtonGrid, self).__init__(**kwargs)
         for i in range(19*19):
             self.add_cell(i)
-            
+        self.state = go.Position()
+        self.intersectionlist = []
+
+    def on_gamestate(self, instance, value):
+        Logger.info('Board state: ' + str(value))
+        for inter in self.intersectionlist:
+            inter_colour = self.gamestate[inter.intersection_id]
+            if inter_colour == go.BLACK:
+                inter.text = 'B'
+            elif inter_colour == go.WHITE:
+                inter.text = 'W'
+            elif inter_colour == go.OPEN:
+                inter.text = ''
+
     def add_cell(self, index):
-        def callback(instance):
-            print('My button {0} is intersection {1}'.format(instance, instance.intersection_id))
+        def make_move(instance):
+            print('The move intersection is {1}'.format(instance, instance.intersection_id))
+            self.state.move(move_pt=instance.intersection_id)
+            print(self.state.board)
+            self.gamestate = self.state.board._board_colour
 
         def _update_loc(inst, value):
             '''Move a child to a new new size/pos. Used by add_cell'''
@@ -45,8 +61,12 @@ class ButtonGrid(GridLayout):
             inst.cover.pos = inst.pos
 
         intersection = Intersection(intersection_id=index)
-        intersection.bind(on_press=callback)
+        intersection.bind(on_press=make_move)
         self.add_widget(intersection)
+        try:
+            self.intersectionlist.append(intersection)
+        except AttributeError:
+            Clock.schedule_once(lambda dt: self.intersectionlist.append(intersection))
 
 
 class Intersection(Button):
